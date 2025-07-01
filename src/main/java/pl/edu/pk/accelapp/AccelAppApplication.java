e169a1331108b9440dc991961edb5b9b8287de63
Aski0 <mlipior79@gmail.com>
2025-07-01 18:19:29 +0200

    init commit

---
+++ src/main/java/pl/edu/pk/accelapp/AccelAppApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.accelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccelAppApplication.class, args);
+    }
+
+}
